ef777823979269aac86632708f82a4ce919c7bc4
Paweł Renc <rencpawe@gmail.com>
2020-12-05 00:04:19 +0100

    Remove redundant presentation part

--- src/miner.jl
+++ src/miner.jl
@@ -10,58 +10,20 @@ using DataStructures: OrderedDict
 using .scoring
 using .evolution
 
-import .evolution: add_event, add_branch, add_loop, crossover
-
 logs = readlines(LOG_FILE)
 letters = join(unique(join(logs)))
-
-println("FOR PRESENTATION PURPOSES")
-println("#########################")
-
-println("Initial regex: ", EXAMPLE_REGEX)
-println("Score: ", score(EXAMPLE_REGEX, logs))
-
-println()
-println("Add event:")
-
-for _ = 1:5
-    println(add_event(EXAMPLE_REGEX, letters))
-end
-
-println()
-println("Add branch:")
-
-for _ = 1:5
-    println(add_branch(EXAMPLE_REGEX, letters))
-end
-
-println()
-println("Add loop:")
-for _ = 1:5
-    println(add_loop(EXAMPLE_REGEX))
-end
-
-println()
-println("Crossover:")
-
-for _ = 1:5
-    println(crossover(EXAMPLE_REGEX, "ab(d(a|g)|(a|g)c)+"))
-end
-
-println("#########################")
-println()
-# algorithm
-println("Actual algorithm")
 
 old_population = init_population(letters, POPULATION_SIZE)
 
 top_rank_list = OrderedDict{String,Float64}(score_population(old_population, logs))
+sort!(top_rank_list, byvalue = true)
 
-for i = 1:ITERATION_NUMBER
+for i in 1:ITERATION_NUMBER
     println("[GENERATION $(i)]")
     global top_rank_list, old_population
 
     new_population = Vector{String}()
+
     println("Top 5 chromosomes:")
     for (i, (chromo, penalty)) in enumerate(pairs(top_rank_list))
         if i <= 5
